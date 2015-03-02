7151c770de646b192b104c06e7dca2f1a27d2f24
squidgetx <squidgetx@gmail.com>
2015-03-01 20:08:39 -0500

    geolocation

--- public/script.js
+++ public/script.js
@@ -9,20 +9,46 @@ var canvas, ctx;
 document.addEventListener("DOMContentLoaded", function() {
   canvas = document.getElementById("canvas");
   ctx = canvas.getContext("2d");
+  ctx.fillStyle = 'blue';
+  ctx.fillRect(0,0,1000,500);
 });
 
 var ws = new WebSocket('ws://localhost:8080');
 
+navigator.geolocation.getCurrentPosition(success, error, options);
+
 ws.onmessage = function (event) {
   data = JSON.parse(event.data);
   updateStats(data);
-  ctx.clearRect(0,0,150,150);
+  ctx.fillStyle = 'blue';
+  ctx.fillRect(0,0,1000,500);
   ctx.fillStyle = 'red';
   data.shapes.forEach(function(s) {
     ctx.fillRect(s.x, s.y, 10, 10);
   });
-  
-
 };
 
+var options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+function success(pos) {
+  var crd = pos.coords;
+  msg = {
+    'lat': crd.latitude,
+    'lon': crd.longitude,
+    'acc': crd.accuracy
+  };
+  ws.send(JSON.stringify(msg));
+};
+
+function error(err) {
+  console.warn('ERROR(' + err.code + '): ' + err.message);
+};
+
+var getLoc = function() {
+  navigator.geolocation.getCurrentPosition(success, error, options);
+}
 
--- server.js
+++ server.js
@@ -12,26 +12,40 @@ var peers = 0;
 
 var shapes = [];
 
+var beinecke = [41.311408, -72.927010];
+
 var wss = new WebSocketServer({server: server});
+
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    client.send(data);
+  });
+}
+
+var id = setInterval(function() {
+  shapes.forEach(function(s) {
+    //s.x += 1;
+  });
+  msg = {
+    'text': 'Connected',
+    'time': new Date(),
+    'peers': peers,
+    'shapes': shapes
+  };
+  wss.broadcast(JSON.stringify(msg), function() {
+    
+  });
+}, 50);
 
 wss.on('connection', function(ws) {
   peers += 1;
-  shapes.push({
-    'x': 10*peers,
-    'y': 10*peers
+  ws.on('message', function(message) {
+    var data = JSON.parse(message);
+    shapes.push({
+      'x': data.lat - beinecke[0] + 500,
+      'y': data.lon - beinecke[1] + 250
+    })
   });
-  var id = setInterval(function() {
-    shapes.forEach(function(s) {
-      s.x += 1;
-    });
-    msg = {
-      'text': 'Connected',
-      'time': new Date(),
-      'peers': peers,
-      'shapes': shapes
-    };
-    ws.send(JSON.stringify(msg));
-  }, 100);
   console.log('started client interval');
   ws.on('close', function() {
     console.log('stopping client interval');
